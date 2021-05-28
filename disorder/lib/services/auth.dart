0e20a88cbdc7f10ff48cba9500edbe441a37b5c8
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-05-28 14:57:39 +0530

    asd

---
+++ disorder/lib/services/auth.dart
@@ -0,0 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+
+class Auth extends ChangeNotifier {
+  final _refrence = FirebaseAuth.instance;
+
+  Stream<UserXE> get onAuthChange {
+    return _refrence.authStateChanges().map(_userDetailsOfFirebase);
+  }
+
+  Future<UserXE> signInEmailandPassword(String email, String password) async {
+    final result = await _refrence.signInWithEmailAndPassword(
+        email: email, password: password);
+    notifyListeners();
+    return _userDetailsOfFirebase(result.user);
+  }
+
+  Future<UserXE> createEmailandPassword(String email, String password) async {
+    final result = await _refrence.createUserWithEmailAndPassword(
+        email: email, password: password);
+    notifyListeners();
+    return _userDetailsOfFirebase(result.user);
+  }
+
+  Future<UserXE> createAnoUser() async {
+    final res = await _refrence.signInAnonymously();
+    return _userDetailsOfFirebase(res);
+  }
+
+  UserXE _userDetailsOfFirebase(user) {
+    if (user == null) {
+      return null;
+    } else {
+      return UserXE(
+          uid: user.uid,
+          email: user.email,
+          isEmailVerified: user.emailVerified);
+    }
+  }
+
+  Future<void> signOut() async {
+    await _refrence.signOut();
+    notifyListeners();
+  }
+}
+
+class UserXE extends ChangeNotifier {
+  final String uid;
+  final String email;
+  bool isEmailVerified = false;
+
+  UserXE({@required this.uid, @required this.email, @required isEmailVerified});
+}
