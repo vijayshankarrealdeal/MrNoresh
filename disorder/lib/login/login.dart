4cdf821da3e42b60eae591c3feda818c74af8594
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-05-28 15:18:42 +0530

    sda

--- disorder/lib/login/login.dart
+++ disorder/lib/login/login.dart
@@ -1,15 +1,32 @@
+import 'dart:js';
+
+import 'package:disorder/services/auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginForApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final auth = Provider.of<Auth>(context);
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/h.jpg'),
+            fit: BoxFit.cover,
           ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(width: double.infinity),
+            CupertinoButton(
+                color: CupertinoColors.black,
+                child: Text("Enter As Guest"),
+                onPressed: () async => await auth.createAnoUser())
+          ],
         ),
       ),
     );
--- disorder/lib/main.dart
+++ disorder/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:disorder/login/login.dart';
+import 'package:disorder/services/auth.dart';
+import 'package:disorder/services/database.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -7,16 +10,37 @@ void main() async {
   Provider.debugCheckInvalidValueType = null;
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context) => Auth()),
+  ], child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(),
-      home: LoginForApp(),
-    );
+    final _auth = Provider.of<Auth>(context);
+    return StreamBuilder<Object>(
+        stream: _auth.onAuthChange,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            UserXE user = snapshot.data;
+            if (user == null) {
+              return MaterialApp(
+                home: LoginForApp(),
+              );
+            } else {
+              return MultiProvider(
+                providers: [
+                  ChangeNotifierProvider<Database>(
+                      create: (context) => Database()),
+                ],
+                child: MyHomePage(),
+              );
+            }
+          } else {
+            return CircularProgressIndicator();
+          }
+        });
   }
 }
 
