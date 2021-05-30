2b390143930b50bc95fc79dc12a589beb8c8e32e
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-05-30 11:02:15 +0530

    dina

--- disorder/lib/app/homepage/data.dart
+++ disorder/lib/app/homepage/data.dart
@@ -16,6 +16,8 @@ class DataSettings extends ChangeNotifier {
     ),
     DataModel(
       title: 'Panic',
+      imageUrl:
+          'https://images.unsplash.com/photo-1515023115689-589c33041d3c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80',
       discription: dummy,
       pastexperience: dummy,
       howtheyarenow: dummy,
